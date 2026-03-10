7b6b2bc8b60e0c0ee5f650f20d1e02332d10adc2
mrb <kristian.w.finstad@gmail.com>
2026-03-10 20:59:41 +0100

    Name search added!

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -7,13 +7,32 @@ import {EmployeeSelectList} from "@/components/employee/EmployeeSelectList";
 import {EmployeeViewData} from "@/components/employee/EmployeeViewData";
 import {AppToolbar} from "@/components/shared/AppToolbar";
 import {ChangeEvent, useState} from "react";
+import {useAppDispatch, useAppSelector} from "@/store/hooks";
+import {employeeViewSlice} from "@/store/slices/employeeViewSlice";
+import {employeeSlice} from "@/store/slices/employeeSlice";
+import {documentGetInitialProps} from "@mui/material-nextjs/v13-pagesRouter";
 
 const EmployeePage = () => {
 
+    const dispatch = useAppDispatch()
+
+    const { setFiltered } = employeeSlice.actions;
+    const allEmployees = useAppSelector(state => state.employees.employees) ?? [];
+
     const [searchInput, setSearchInput] = useState<string>("");
+
     const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
         const inputData = event.target.value;
         setSearchInput(inputData);
+
+        const searchParts = inputData.toLowerCase().split(" ").filter(Boolean)
+
+        const searchResult = allEmployees.filter(e => {
+            const nameNorm = e.Name.toLowerCase();
+            return searchParts.every(part => nameNorm.includes(part));
+        })
+
+        dispatch(setFiltered(searchResult));
     }
 
     return <AppPageContainer title="Ansatte">
@@ -24,20 +43,7 @@ const EmployeePage = () => {
         <Box sx={{display: "flex", flexDirection: "row"}}>
             <EmployeeSelectList />
             <EmployeeViewData />
-
         </Box>
-
-        <div>
-            <p>Denne siden skal inneholde forskjellige data per ansatt. Den skal inneholde følgende features:</p>
-            <ul>
-                <li>
-                    Heatmap for antall vakter fordelt på uker. Type senvakt på fredager og normalvakt på fredag
-                </li>
-                <li>
-                    Antall endrede vakter
-                </li>
-            </ul>
-        </div>
     </AppPageContainer>
 }
 
